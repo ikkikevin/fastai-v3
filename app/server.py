13ee41f68e7c109c329410ae33efe7f91cc52b6f
MisterT098 <64009045+MisterT098@users.noreply.github.com>
2020-06-03 16:37:56 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,12 +61,12 @@ async def homepage(request):
 async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    image = numpy.float32(img)
+    image = cv2.imread(img_bytes)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     th3 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
                                cv2.THRESH_BINARY, 11, 4)
-    prediction = learn.predict(th3)[0]
+    img = open_image(BytesIO(th3))
+    prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
 
