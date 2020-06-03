40cf6c8ce1bbcf404e85080148d0220d3f6bbf38
MisterT098 <64009045+MisterT098@users.noreply.github.com>
2020-06-03 14:23:59 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -1,125 +1,40 @@
-var isSecureOrigin = location.protocol === 'https:' ||
-location.host === 'localhost';
-if (!isSecureOrigin) {
-  alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
-    '\n\nChanging protocol to HTTPS');
-  location.protocol = 'HTTPS';
+var el = x => document.getElementById(x);
+
+function showPicker() {
+  el("file-input").click();
 }
 
-var constraints;
-var imageCapture;
-var mediaStream;
-
-var grabFrameButton = document.querySelector('button#grabFrame');
-var takePhotoButton = document.querySelector('button#takePhoto');
-
-var canvas = document.querySelector('canvas');
-var img = document.querySelector('img');
-var video = document.querySelector('video');
-var videoSelect = document.querySelector('select#videoSource');
-var zoomInput = document.querySelector('input#zoom');
-
-grabFrameButton.onclick = grabFrame;
-takePhotoButton.onclick = takePhoto;
-videoSelect.onchange = getStream;
-zoomInput.oninput = setZoom;
-
-// Get a list of available media input (and output) devices
-// then get a MediaStream for the currently selected input device
-navigator.mediaDevices.enumerateDevices()
-  .then(gotDevices)
-  .catch(error => {
-    console.log('enumerateDevices() error: ', error);
-  })
-  .then(getStream);
-
-// From the list of media devices available, set up the camera source <select>,
-// then get a video stream from the default camera source.
-function gotDevices(deviceInfos) {
-  for (var i = 0; i !== deviceInfos.length; ++i) {
-    var deviceInfo = deviceInfos[i];
-    console.log('Found media input or output device: ', deviceInfo);
-    var option = document.createElement('option');
-    option.value = deviceInfo.deviceId;
-    if (deviceInfo.kind === 'videoinput') {
-      option.text = deviceInfo.label || 'Camera ' + (videoSelect.length + 1);
-      videoSelect.appendChild(option);
-    }
-  }
-}
-
-// Get a video stream from the currently selected camera source.
-function getStream() {
-  if (mediaStream) {
-    mediaStream.getTracks().forEach(track => {
-      track.stop();
-    });
-  }
-  var videoSource = videoSelect.value;
-  constraints = {
-    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+function showPicked(input) {
+  el("upload-label").innerHTML = input.files[0].name;
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    el("image-picked").src = e.target.result;
+    el("image-picked").className = "";
   };
-  navigator.mediaDevices.getUserMedia(constraints)
-    .then(gotStream)
-    .catch(error => {
-      console.log('getUserMedia error: ', error);
-    });
+  reader.readAsDataURL(input.files[0]);
 }
 
-// Display the stream from the currently selected camera source, and then
-// create an ImageCapture object, using the video from the stream.
-function gotStream(stream) {
-  console.log('getUserMedia() got stream: ', stream);
-  mediaStream = stream;
-  video.srcObject = stream;
-  video.classList.remove('hidden');
-  imageCapture = new ImageCapture(stream.getVideoTracks()[0]);
-  getCapabilities();
-}
+function analyze() {
+  var uploadFiles = el("file-input").files;
+  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-// Get the PhotoCapabilities for the currently selected camera source.
-function getCapabilities() {
-  imageCapture.getPhotoCapabilities().then(function(capabilities) {
-    console.log('Camera capabilities:', capabilities);
-    if (capabilities.zoom.max > 0) {
-      zoomInput.min = capabilities.zoom.min;
-      zoomInput.max = capabilities.zoom.max;
-      zoomInput.value = capabilities.zoom.current;
-      zoomInput.classList.remove('hidden');
+  el("analyze-button").innerHTML = "Analyzing...";
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Result = ${response["result"]}`;
     }
-  }).catch(function(error) {
-    console.log('getCapabilities() error: ', error);
-  });
-}
+    el("analyze-button").innerHTML = "Analyze";
+  };
 
-// Get an ImageBitmap from the currently selected camera source and
-// display this with a canvas element.
-function grabFrame() {
-  imageCapture.grabFrame().then(function(imageBitmap) {
-    console.log('Grabbed frame:', imageBitmap);
-    canvas.width = imageBitmap.width;
-    canvas.height = imageBitmap.height;
-    canvas.getContext('2d').drawImage(imageBitmap, 0, 0);
-    canvas.classList.remove('hidden');
-  }).catch(function(error) {
-    console.log('grabFrame() error: ', error);
-  });
-}
-
-function setZoom() {
-  imageCapture.setOptions({
-    zoom: zoomInput.value
-  });
-}
-
-// Get a Blob from the currently selected camera source and
-// display this with an img element.
-function takePhoto() {
-  imageCapture.takePhoto().then(function(blob) {
-    console.log('Took photo:', blob);
-    img.classList.remove('hidden');
-    img.src = URL.createObjectURL(blob);
-  }).catch(function(error) {
-    console.log('takePhoto() error: ', error);
-  });
+  var fileData = new FormData();
+  fileData.append("file", uploadFiles[0]);
+  xhr.send(fileData);
 }
