d1a7c22c69eb6e5322a66d17ca97b7b247a0371c
ikkikevin <57066897+ikkikevin@users.noreply.github.com>
2020-06-11 12:02:01 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -1,12 +1,166 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var isSecureOrigin = location.protocol === 'https:' ||
+location.host === 'localhost';
+if (!isSecureOrigin) {
+  alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
+    '\n\nChanging protocol to HTTPS');
+  location.protocol = 'HTTPS';
+}
+
+var constraints;
+var mediaStream;
+
+
+var video = document.querySelector('video');
+var videoSelect = document.querySelector('select#videoSource');
+var zoomInput = document.querySelector('input#zoom');
+
+videoSelect.onchange = getStream;
+zoomInput.oninput = setZoom;
+
+// Get a list of available media input (and output) devices
+// then get a MediaStream for the currently selected input device
+navigator.mediaDevices.enumerateDevices()
+  .then(gotDevices)
+  .catch(error => {
+    console.log('enumerateDevices() error: ', error);
+  })
+  .then(getStream);
+
+// From the list of media devices available, set up the camera source <select>,
+// then get a video stream from the default camera source.
+function gotDevices(deviceInfos) {
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i];
+    console.log('Found media input or output device: ', deviceInfo);
+    var option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'Camera ' + (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    }
+  }
+}
+
+// Get a video stream from the currently selected camera source.
+function getStream() {
+  if (mediaStream) {
+    mediaStream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  var videoSource = videoSelect.value;
+  constraints = {
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(gotStream)
+    .catch(error => {
+      console.log('getUserMedia error: ', error);
+    });
+}
+
+// Display the stream from the currently selected camera source, and then
+// create an ImageCapture object, using the video from the stream.
+function gotStream(stream) {
+  console.log('getUserMedia() got stream: ', stream);
+  mediaStream = stream;
+  video.srcObject = stream;
+  video.classList.remove('hidden');
+  imageCapture = new ImageCapture(stream.getVideoTracks()[0]);
+  getCapabilities();
+}
+
+// Get the PhotoCapabilities for the currently selected camera source.
+function getCapabilities() {
+  imageCapture.getPhotoCapabilities().then(function(capabilities) {
+    console.log('Camera capabilities:', capabilities);
+    if (capabilities.zoom.max > 0) {
+      zoomInput.min = capabilities.zoom.min;
+      zoomInput.max = capabilities.zoom.max;
+      zoomInput.value = capabilities.zoom.current;
+      zoomInput.classList.remove('hidden');
+    }
+  }).catch(function(error) {
+    console.log('getCapabilities() error: ', error);
+  });
+}
+
+
+
+function setZoom() {
+  imageCapture.setOptions({
+    zoom: zoomInput.value
+  });
+}
+
+
+    
+	
+	var imageCapture;
+var grabFrameButton = document.querySelector('button#grabFrame');
+var takePhotoButton = document.querySelector('button#takePhoto');
+
+grabFrameButton.onclick = grabFrame;
+takePhotoButton.onclick = takePhoto;
+
+var canvas = document.querySelector('canvas');
+var img = document.querySelector('img');
+
+	
+	
+// Get an ImageBitmap from the currently selected camera source and
+// display this with a canvas element.
+function grabFrame() {
+  imageCapture.grabFrame().then(function(imageBitmap) {
+    console.log('Grabbed frame:', imageBitmap);
+    canvas.width = imageBitmap.width;
+    canvas.height = imageBitmap.height;
+    canvas.getContext('2d').drawImage(imageBitmap, 0, 0);
+    canvas.classList.remove('hidden');
+  }).catch(function(error) {
+    console.log('grabFrame() error: ', error);
+  });
+}
+
+
+// Get a Blob from the currently selected camera source and
+// display this with an img element.
+function takePhoto() {
+  imageCapture.takePhoto().then(function(blob) {
+    console.log('Took photo:', blob);
+    img.classList.remove('hidden');
+    img.src = URL.createObjectURL(blob);
+	  foto = img.src;
+  }).catch(function(error) {
+    console.log('takePhoto() error: ', error);
+  });
+}
+ 
+});
+   
+   var foto;
+
+  function myFunction() {
+      document.getElementById("demo").innerHTML = foto;
+  }
+
+
+
+
+
+
+
+
+
 var el = x => document.getElementById(x);
 
 function showPicker() {
-  var input = 'https://imgn.rgcdn.nl/5cc133dc43194047a432cf6ac9a088c1/opener/Niet-de-kip-in-kwestie-Foto-gvvjr007.jpg'
   el("file-input").click();
 }
 
 function showPicked(input) {
-  var input = 'https://imgn.rgcdn.nl/5cc133dc43194047a432cf6ac9a088c1/opener/Niet-de-kip-in-kwestie-Foto-gvvjr007.jpg'
   el("upload-label").innerHTML = input.files[0].name;
   var reader = new FileReader();
   reader.onload = function(e) {
@@ -14,11 +168,6 @@ function showPicked(input) {
     el("image-picked").className = "";
   };
   reader.readAsDataURL(input.files[0]);
-}
-
-
-function myFunction() {
- var uploadFiles = el('file-input').files;
 }
 
 
