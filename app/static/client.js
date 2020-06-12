c0997ad0d5e8e9c3a603b445b04769403650c25b
ikkikevin <57066897+ikkikevin@users.noreply.github.com>
2020-06-12 12:50:31 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -165,8 +165,8 @@ function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
   var reader = new FileReader();
   reader.onload = function(e) {
-    el("image-picked").src = e.target.result;
-    el("image-picked").className = "";
+    el("image-picked").src = dataURL;
+    el("image-picked").className = e;
   };
   reader.readAsDataURL(input.files[0]);
 }
