edf2846b58adc84598dc4cd300fb86ee339fb86a
ikkikevin <57066897+ikkikevin@users.noreply.github.com>
2020-06-14 00:50:37 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -210,6 +210,25 @@ function b64toBlob(b64Data, contentType, sliceSize) {
 }
 
 
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
 
 
 
@@ -230,7 +249,7 @@ function analyze() {
   $('i#iconcancel').hide();
   $('a#analyze-buttonDisabled').show();
   $('a#analyze-button').hide();
-  el("result-label").innerHTML = "Please wait. This could take up to 30 seconds.";
+  el("result-label").innerHTML = "Please wait. This could take up to 30 seconds.<br/><label id='minutes'>00</label>:<label id='seconds'>00</label>";
   el("analyzetext").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
